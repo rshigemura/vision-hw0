1670981ed111425e388198a7c4e0c7921fef84e2
rafael.shigemura <rafael.shigemura@gmail.com>
2019-07-22 14:53:09 -0300

    Homework 0 code

--- tryit.py
+++ tryit.py
@@ -4,24 +4,29 @@ from uwimg import *
 im = load_image("data/dog.jpg")
 for row in range(im.h):
     for col in range(im.w):
-        set_pixel(im, col, row, 0, 0)
-save_image(im, "dog_no_red")
+        set_pixel(im, col, row, 1, 0)
+save_image(im, "figs/results_rafael/dog_no_green")
 
 # 3. Grayscale image
 im = load_image("data/colorbar.png")
 graybar = rgb_to_grayscale(im)
-save_image(graybar, "graybar")
+save_image(graybar, "figs/results_rafael/graybar")
+
+# 3. Grayscale image II
+im = load_image("data/dog.jpg")
+graybar = rgb_to_grayscale(im)
+save_image(graybar, "figs/results_rafael/graydog")
 
 # 4. Shift Image
 im = load_image("data/dog.jpg")
 shift_image(im, 0, .4)
 shift_image(im, 1, .4)
 shift_image(im, 2, .4)
-save_image(im, "overflow")
+save_image(im, "figs/results_rafael/overflow")
 
 # 5. Clamp Image
 clamp_image(im)
-save_image(im, "doglight_fixed")
+save_image(im, "figs/results_rafael/doglight_fixed")
 
 # 6-7. Colorspace and saturation
 im = load_image("data/dog.jpg")
@@ -29,6 +34,14 @@ rgb_to_hsv(im)
 shift_image(im, 1, .2)
 clamp_image(im)
 hsv_to_rgb(im)
-save_image(im, "dog_saturated")
+save_image(im, "figs/results_rafael/dog_saturated")
+
+# 8. Channel scalling
+im = load_image("data/dog.jpg")
+rgb_to_hsv(im)
+scale_image(im, 1, 2)
+clamp_image(im)
+hsv_to_rgb(im)
+save_image(im,"figs/results_rafael/dog_scale_saturated_rafael")
 
 
--- uwimg.py
+++ uwimg.py
@@ -53,6 +53,9 @@ load_image_lib = lib.load_image
 load_image_lib.argtypes = [c_char_p]
 load_image_lib.restype = IMAGE
 
+scale_image = lib.scale_image
+scale_image.argtypes = [IMAGE, c_int, c_float]
+
 def load_image(f):
     return load_image_lib(f.encode('ascii'))
 
@@ -61,10 +64,8 @@ save_image_lib.argtypes = [IMAGE, c_char_p]
 
 def save_image(im, f):
     return save_image_lib(im, f.encode('ascii'))
+    
 
 if __name__ == "__main__":
     im = load_image("data/dog.jpg")
     save_image(im, "hey")
-
-    
-
